87ddf2bad48b232de5ed9e966f763465cce82715
Ursescu Ionut <ursescuionut97@gmail.com>
2020-12-26 16:34:31 +0200

    Added macro to read from the buffer in STK command handler.

--- stk500/misc.h
+++ stk500/misc.h
@@ -1,13 +1,19 @@
 #ifndef _MISC_H_
 #define _MISC_H_
 
-#include <avr/io.h>
+#include <stdint.h>
 
 #define WRITE_BUF8(buffer, data, size)              \
     do                                              \
     {                                               \
         *((uint8_t *)(buffer)++) = (uint8_t)(data); \
         (size) += 1;                                \
+    } while (0)
+
+#define READ_BUF8(buffer, data)                     \
+    do                                              \
+    {                                               \
+        (data) = *((uint8_t *)(buffer)++); \
     } while (0)
 
 #endif
